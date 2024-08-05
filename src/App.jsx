d0e6f98cd11d461ee36fe9b995d9064443b5f3cc
Saibot2001 <tobiascomas81@gmail.com>
2024-08-04 21:48:18 -0300

    Mensaje del commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar.jsx'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
+import ItemCount from './components/ItemCount/ItemCount.jsx';
+
 function App() {
 
   return  (
     <div className="App">
       <NavBar /> 
       <ItemListContainer greeting={'Bienvenidos'}/>
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada', quantity)}/>
     </div>
   )
   
---
+++ src/asyncMock.jsx
@@ -0,0 +1,37 @@
+const products = [
+    {
+        id: '1',
+        name: ' producto',
+        price: 100,
+        category: 'celular',
+        img: 'pendiente',
+        stock: 25,
+        description: 'dfjdklfjdkfjd'
+    },
+    {
+        id: '2',
+        name: ' producto2',
+        price: 100,
+        category: 'celular',
+        img: 'pendiente',
+        stock: 25,
+        description: 'dfjdklfjdkfjd'
+    },
+    {
+        id: '3',
+        name: ' producto3',
+        price: 100,
+        category: 'celular',
+        img: 'pendiente',
+        stock: 25,
+        description: 'dfjdklfjdkfjd'
+    },
+]
+
+export const getProducts =() => {
+    return new Promise ((resolve) => {
+        setTimeout(() =>{
+            resolve(products)
+        }, 500)
+    })
+}
